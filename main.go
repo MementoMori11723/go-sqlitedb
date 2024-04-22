14e87f001c04a1c43ccc911654e7fd1206e8f3ec
yasasvi <yasasvigumma@gmail.com>
2024-04-22 18:54:47 +0530

    creating table in db

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 	
 
 func main() {
-	db, err := sql.Open("sqlite3", "sqlite:data.db")
+	db, err := sql.Open("sqlite3", "data.db")
 	if err != nil {
 		panic(err)
 	}
 	log.Println("Connected to the database")
+	db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)")
+	log.Println("Created table users")
 	defer db.Close()
 }
